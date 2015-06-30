0458b5e95d56fdcfa52fb1825a430dc7e3581df2
Jeremy B. Merrill <jeremy.merrill@nytimes.com>
2015-06-30 15:17:28 -0400

    notify about last week and week before, since they never have 'this week' up

--- src/bin/status_checker.rb
+++ src/bin/status_checker.rb
@@ -47,8 +47,8 @@ if __FILE__ == $0
   last_few_reports = weeks_we_have.map{|row| row['start_date'].to_s[0...10] + " - " + row['end_date'].to_s[0...10]}[0...4]
 
   # check for this week and last week
-  this_week = Time.now
   last_week = Time.now - 7.days
+  two_weeks_ago = Time.now - 14.days
 
   messages = []
   okay_messages = []
@@ -56,30 +56,30 @@ if __FILE__ == $0
   subject_items = []
 
   # compose a message if this week is missing its reports  
-  if (missing_cnt = is_missing_compstats(this_week)) > 0
+  if (missing_cnt = is_missing_compstats(last_week)) > 0
     if missing_cnt < EXPECTED_REPORTS
-      messages << "Yikes! #{missing_cnt} reports are missing from the compstat scraper for this week (ending #{this_week}). Or maybe something went wrong)."
+      messages << "Yikes! #{missing_cnt} reports are missing from the compstat scraper for this week (ending #{last_week}). Or maybe something went wrong)."
       subject_emoji ||= "❓👮📉 CompStat:"
-      subject_items << "missing reports for this week (#{this_week.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
+      subject_items << "missing reports for this week (#{last_week.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
     else
-      messages << "Uh oh! There are no compstat reports for this week (ending #{this_week}). Or maybe something went wrong..."
+      messages << "Uh oh! There are no compstat reports for this week (ending #{last_week}). Or maybe something went wrong..."
       subject_emoji ||= "😖👮📉 CompStat:"
-      subject_items << "no reports for this week (#{this_week.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
+      subject_items << "no reports for this week (#{last_week.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
     end
   else
     okay_messages << "but THIS week's compstat reports okay."  
   end
 
   # compose a message if last week is missing its reports  
-  if ((missing_cnt = is_missing_compstats(last_week)) > 0 && last_week > Date.new(2015,1,6))
+  if ((missing_cnt = is_missing_compstats(two_weeks_ago)) > 0 && two_weeks_ago > Date.new(2015,1,6))
     if missing_cnt < EXPECTED_REPORTS
-      messages << "Pick up the phone! #{missing_cnt} reports are missing from the compstat scraper for last week (ending #{last_week}). Or maybe something went wrong."
+      messages << "Pick up the phone! #{missing_cnt} reports are missing from the compstat scraper for last week (ending #{two_weeks_ago}). Or maybe something went wrong."
       subject_emoji ||= "📞👮📉 CompStat:"
-      subject_items << "missing reports for LAST week (#{last_week.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
+      subject_items << "missing reports for LAST week (#{two_weeks_ago.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
     else
-      messages << "Jinkies! There are no compstat reports for LAST week (ending #{last_week}). Or maybe something went wrong..."
+      messages << "Jinkies! There are no compstat reports for LAST week (ending #{two_weeks_ago}). Or maybe something went wrong..."
       subject_emoji ||= "💢👮📉 CompStat:"
-      subject_items << "no reports for LAST week (#{last_week.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
+      subject_items << "no reports for LAST week (#{two_weeks_ago.to_s[5..9].gsub('-', '/').gsub(/^0*/, '')})"  # only used for email.
     end
   else
     okay_messages << "but LAST week's compstat reports okay."  
@@ -92,8 +92,6 @@ if __FILE__ == $0
   if messages.empty?
     subject = "👮: everything is copacetic within the confines of the compstat precinct"
     message = "omg lol"
-  else
-    puts "asdf"
   end
   snes.publish(message , {subject: subject} ) unless messages.empty?
 end
